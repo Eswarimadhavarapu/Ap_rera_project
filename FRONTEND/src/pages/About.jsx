e2a4076bc9d00537523e74603c5b9d39b3c0caf1
Eswari Madhavarapu <your-github-email@example.com>
2026-02-23 10:50:44 +0530

    2302

--- FRONTEND/src/pages/About.jsx
+++ FRONTEND/src/pages/About.jsx
@@ -2,6 +2,7 @@ const About = () => {
   return (
     <>
       <h2>About AP RERA</h2>
+      <h2>About AP RERA</h2>
       <p>
         Andhra Pradesh Real Estate Regulatory Authority regulates real estate
         sector and protects home buyers.
